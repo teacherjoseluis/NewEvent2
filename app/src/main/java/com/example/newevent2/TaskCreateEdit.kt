fb94e78d76812d661babb8fdea0acf6f19643910
Jose Luis Cruz <teacherjoseluis@gmail.com>
2022-11-02 13:44:15 -0600

    Push on 2/11/2022

--- app/src/main/java/com/example/newevent2/TaskCreateEdit.kt
+++ app/src/main/java/com/example/newevent2/TaskCreateEdit.kt
@@ -12,6 +12,7 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
